8e938ec6634f59ac29f0acb234347484028dfed4
Zabbara Maximilien <max2759@gmail.com>
2020-03-20 15:55:20 +0100

    Connexion & deconnexion + restriction accès si pas co - design change

--- VIEW/home.php
+++ VIEW/home.php
@@ -3,11 +3,7 @@
 
 if ($_SESSION['isLogged']) {
     echo '<p> Bienvenue '.$_SESSION['pseudoLog'] .'</p>';
-    echo '
-        <form method="post" action="../CONTROL/Deconnexion.php">
-		<input type="submit" value="Se déconnecter" name="Deconnexion" >
-		
-		</form>';
+
 }else {
     header('Location: ../CONTROL/connexion.php');
     exit();
