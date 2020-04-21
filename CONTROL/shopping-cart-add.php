2e1cd3c09b6a05e858ce829692350ca79c56bd25
Zabbara Maximilien <max2759@gmail.com>
2020-04-21 20:26:23 +0200

    panier fetch success

--- CONTROL/shopping-cart-add.php
+++ CONTROL/shopping-cart-add.php
@@ -6,7 +6,7 @@ $cars = model::load('cars');
 $basket = model::load('shoppingCart');
 $carsID = $_POST['carsId'];
 
-$products = $cars->query('SELECT b.name, c.cars_ID, c.model, c.picture, c.unitPrice from cars c inner join brands b on b.brands_ID = c.brands_ID where c.cars_ID = :cars_ID', array('cars_ID'=>$carsID));
+$products = $cars->query('SELECT b.name, c.cars_ID, c.model, c.picture, c.unitprice from cars c inner join brands b on b.brands_ID = c.brands_ID where c.cars_ID = :cars_ID', array('cars_ID'=>$carsID));
 
 $_SESSION['panier'] = $products;
 
--- CONTROL/shopping-cart.php
+++ CONTROL/shopping-cart.php
@@ -8,9 +8,9 @@ require_once('core.php');
 if (isset($_SESSION['isAdmin'])) {
     require('../VIEW/Form.php');
     require('../VIEW/header.php');
-    require('../VIEW/shopping-cart.php');
     $basket = model::load("shoppingCart");
     $cars = model::load("cars");
+    require('../VIEW/shopping-cart.php');
     require('../VIEW/footer.php');
 } else {
     header('Location: connexion.php');
